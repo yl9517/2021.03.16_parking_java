1323b4a0a366aab50c3b28988fb0555f79b9570c
yl9517 <80736033+yl9517@users.noreply.github.com>
2021-03-27 12:53:44 +0900

    Delete src directory

--- src/DBconnect/config.java
+++
@@ -1,9 +0,0 @@
-package DBconnect;
-
-public class config {
-	protected String dbhost = "localhost";
-	protected String dbport = "3306";
-	protected String dbuser = "root";
-	protected String dbpass = "1234";
-	protected String dbname = "park";
-}
